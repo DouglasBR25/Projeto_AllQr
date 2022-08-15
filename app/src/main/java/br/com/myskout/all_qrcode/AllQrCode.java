beaf2d6d9f03ad14b408cf5fdac6d10635a41460
DouglasBR25 <douglas2502@gmail.com>
2022-08-15 11:37:14 -0300

    first commit

---
+++ app/src/main/java/br/com/myskout/all_qrcode/AllQrCode.java
@@ -0,0 +1,14 @@
+package br.com.myskout.all_qrcode;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AllQrCode extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_qr_code);
+    }
+}
